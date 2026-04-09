911838a253b1178c91a03267b5d14d2f72c0ddde
Gina Gunay Healy <125167099+GinaGH@users.noreply.github.com>
2026-04-08 19:53:12 -0700

    Update demo.py

--- examples/demo.py
+++ examples/demo.py
@@ -18,10 +18,18 @@ src_path = os.path.join(current_dir, '..', 'src')
 # 3. Add that calculated path to Python's search list
 sys.path.append(os.path.abspath(src_path))
 
-from kdtree import build_kdtree
+from kdtree import build_kdtree, nearest_neighbor
 import random
 
-points = [(random.random(), random.random()) for _ in range(10)]
-tree = build_kdtree(points)
 
+# Construct a k-d tree 
+points = [(random.random(), random.random()) for _ in range(20)]
+tree = build_kdtree(points)
 print("KD-tree constructed with", len(points), "points")
+
+# find the nearest neighbor to the target point
+target = (0.5, 0.5)
+nn = nearest_neighbor(tree, target)
+
+print("Target:", target)
+print("Nearest neighbor:", nn)
