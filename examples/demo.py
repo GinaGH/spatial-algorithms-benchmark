a71e78937fc5a8f8cd92d2146b183e98e99ebb5a
Gina Gunay Healy <125167099+GinaGH@users.noreply.github.com>
2026-04-08 19:53:50 -0700

    Update demo.py
    add nearest neighbor search to KD-tree

--- examples/demo.py
+++ examples/demo.py
@@ -8,6 +8,8 @@
 
 import sys
 import os
+from kdtree import build_kdtree, nearest_neighbor
+import random
 
 # 1. Get the directory where your current script is located
 current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -17,10 +19,6 @@ src_path = os.path.join(current_dir, '..', 'src')
 
 # 3. Add that calculated path to Python's search list
 sys.path.append(os.path.abspath(src_path))
-
-from kdtree import build_kdtree, nearest_neighbor
-import random
-
 
 # Construct a k-d tree 
 points = [(random.random(), random.random()) for _ in range(20)]
