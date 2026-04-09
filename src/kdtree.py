efc7523a6457e5f80bad7267b4820548553d26a1
Gina Gunay Healy <125167099+GinaGH@users.noreply.github.com>
2026-04-08 19:24:05 -0700

    Create kdtree.py

---
+++ src/kdtree.py
@@ -0,0 +1,29 @@
+#
+# Gina G. Healy
+# KD-tree 
+#
+
+class KDNode:
+    def __init__(self, point, axis, left=None, right=None):
+        self.point = point
+        self.axis = axis
+        self.left = left
+        self.right = right
+
+
+def build_kdtree(points, depth=0):
+    if not points:
+        return None
+
+    k = len(points[0])  # dimension
+    axis = depth % k
+
+    points.sort(key=lambda x: x[axis])
+    median = len(points) // 2
+
+    return KDNode(
+        point=points[median],
+        axis=axis,
+        left=build_kdtree(points[:median], depth + 1),
+        right=build_kdtree(points[median + 1:], depth + 1)
+    )
